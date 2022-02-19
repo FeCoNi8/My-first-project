8837c84e8662a9105561d9a99c2b39fd797b10f7
FeCoNi8 <yumzhik2021@gmail.com>
2022-02-19 21:39:52 +0800

    creat caltulator of the equation

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello world")
+import math
+def calculate_fun(a,b,c):
+    D = b **2 - 4*a*c
+    if D > 0:
+        x1 = (- b + math.sqrt(D)) /2*a
+        x2 = (-b - math.sqrt(D)) / 2*a
+        return x1, x2
+    elif D == 0:
+        x = (- b) / 2 * a
+        return x
+    else:
+        return False
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+result = calculate_fun(a,b,c)
+print(result)
+
+
+
+
+
