928e0f8337ea77cd4c7ffcf29e05abf48b97e058
FeCoNi8 <yumzhik2021@gmail.com>
2022-02-20 17:00:14 +0800

    create several function

--- get_currency_data.py
+++ get_currency_data.py
@@ -1,9 +1,25 @@
 import requests
+import json
 
 
 def parse_site(url):
     response = requests.get(url)
-    return response
+    return response.json()
 
 url = "https://www.cbr-xml-daily.ru/daily_json.js"
-print(parse_site(url))
+
+
+
+def get_json_data(url):
+    data_json = parse_site(url)
+    return data_json
+
+url = "https://www.cbr-xml-daily.ru/daily_json.js"
+def main():
+    with open('data_json.json', 'r') as f:
+        data_json = json.load(f)
+        print(data_json)
+main()
+
+
+
