c0c5e723619439910657ebadc12a829e5552e7b9
Azamat <azamatkomaev@mail.ru>
2022-02-20 12:21:44 +0300

    Update get_currency_data.py

--- get_currency_data.py
+++ get_currency_data.py
@@ -2,24 +2,53 @@ import requests
 import json
 
 
-def parse_site(url):
+def get_json_from_site(url):
+    """Get dict from site."""
     response = requests.get(url)
     return response.json()
 
-url = "https://www.cbr-xml-daily.ru/daily_json.js"
+
+def write_json_data_in_file(file_name, json_data):
+    """Write json data in file."""
+    with open(file_name, 'w') as f:
+        json.dump(json_data, f)
 
 
+def read_json_data_in_file(file_name):
+    """Read json data from file."""
+    with open(file_name, 'r') as f:
+        return json.load(f)
 
-def get_json_data(url):
-    data_json = parse_site(url)
-    return data_json
 
-url = "https://www.cbr-xml-daily.ru/daily_json.js"
 def main():
-    with open('data_json.json', 'r') as f:
-        data_json = json.load(f)
-        print(data_json)
-main()
+    """The main function."""
+    url = "https://www.cbr-xml-daily.ru/daily_json.js"
+    action = input('Where should i get data? (0-from file, 1-from internet)>>>')
+    if action == "1":
+        """
+        If user choose action for getting data from internet. 
+        Here, i add data from inet in data_json.json file.
+        """
+        json_data = get_json_from_site(url)
+        write_json_data_in_file('data_json.json', json_data)
 
+        print(f'File was created/updated! Run and choose from file.')
+        return
+
+    elif action == "0":
+        """If user choose action for getting data from file. Just get data from file, l-logic."""
+        json_data = read_json_data_in_file('data_json.json')
+
+        print(json_data)
+        return
+
+    else:
+        print('You chose a wrong action.')
+        return
+
+
+if __name__ == "__main__":
+    """Точка входа нашей программы."""
+    main()
 
 
