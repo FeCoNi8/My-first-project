edcccd4489db96e15fd7fb27ce116d9352e94cde
FeCoNi8 <yumzhik2021@gmail.com>
2022-02-20 18:40:25 +0800

    create new function

--- get_currency_data.py
+++ get_currency_data.py
@@ -20,8 +20,23 @@ def read_json_data_in_file(file_name):
         return json.load(f)
 
 
+def parse_valley(initial, data):
+    """Parse_valley prom json by initial"""
+    try:
+        valley_json = data['Valute'][initial]
+        return valley_json
+    except KeyError:
+        return "None"
+
+
+def print_valley(valley_data):
+    return f'ID:{valley_data["ID"]} \ninitial: {valley_data["CharCode"]} \nName:{valley_data ["Name"]} \nPrice:{valley_data["Value"]} rubles'
+
+
 def main():
     """The main function."""
+    json_data = None
+
     url = "https://www.cbr-xml-daily.ru/daily_json.js"
     action = input('Where should i get data? (0-from file, 1-from internet)>>>')
     if action == "1":
@@ -33,18 +48,23 @@ def main():
         write_json_data_in_file('data_json.json', json_data)
 
         print(f'File was created/updated! Run and choose from file.')
-        return
+
 
     elif action == "0":
         """If user choose action for getting data from file. Just get data from file, l-logic."""
         json_data = read_json_data_in_file('data_json.json')
 
-        print(json_data)
-        return
 
     else:
         print('You chose a wrong action.')
         return
+
+
+    initials = input('Enter several initials: >>>').split(',')
+    for initial in initials:
+        valley_data = parse_valley(initial,json_data)
+        print(print_valley(valley_data))
+        print("")
 
 
 if __name__ == "__main__":
